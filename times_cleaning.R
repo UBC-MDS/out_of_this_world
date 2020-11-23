ea9f9ad190992555c2736c4afcac8f9247832d75
jacobmcf <jacobmcf@student.ubc.ca>
2020-11-23 08:35:11 -0800

    Refactor tests

--- times_cleaning.R
+++ times_cleaning.R
@@ -4,13 +4,13 @@ library(tidyverse)
 
 #' Cleans times
 #'
-#' This is a function to take times inputed by users of online forms without any restrictions
+#' This is a function to take times inputted by users of online forms without any restrictions
 #' and converts them to seconds
 #' 
 #' @param df 
 #' @param column 
 #'
-#' @return
+#' @return df 
 #' @export
 #'
 #' @examples
@@ -18,18 +18,35 @@ clean_times <- function(df, col_to_clean, clean_col_name){
   return(df)
 }
 
+
 # Tests
-# Minutes
-dirty_minutes <- c("2 minutes", "5 mins", "10 min.", "1 minute")
-clean_minutes <- c(120, 300, 600, 60)
+# Setting up test data
+dirty_times <- c("2 minutes", "5 mins", "10 min.", "1 minute", "25 MINS",
+                 "2 hours", "5 hours", "10 hour.", "1 HOUR", "25 hours",
+                 "1 second", "5 seconds", "10. seconds", "5 SEC", "120 SECS",
+                 "~1 sec", "1-2 mins", "all night", "now", "ten seconds!!!", "<10 secs",
+                 ">10 secs", "almost 10 secs", "7:45-9:21", "1 hour approx.")
 
-minute_tests <- tibble(dmins = dirty_minutes, 
-                       cmins = clean_minutes)
+labels <- rep(c("minute", "hour", "second", "NA", "NA"), each = 5)
 
-clean_minute_tests <- clean_times(minute_tests, dmins, ct_mins)
+clean_times <- c(120, 300, 600, 60, (25 * 60),
+                 2 * 3600, 5 * 3600, 10 * 3600, 1 * 3600, 25 * 3600,
+                 1, 5, 10, 5, 120,
+                 rep(NA, 10))
 
-test_that("The minutes are converted per specifications",
+test_tib <- tibble(dirty_times = dirty_times, labels = labels, clean_times = clean_times)
+
+# Does the function handle minutes well?
+
+test_mins <- test_tib %>%
+  filter(labels == "minute")
+
+test_that("Function converts minutes well",
           expect_equal())
+
+
+
+
 
 
 # # basic cleaning
